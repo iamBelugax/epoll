2462c4ee3297b495c43387b340c84265fb9e518a
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-06-29 13:52:51 +0530

    add defaults

---
+++ internal/server/defaults.go
@@ -0,0 +1,10 @@
+package server
+
+var (
+	// Maximum number of events to retrieve from epoll_wait per goroutine.
+	MAX_EVENTS = 1024
+	// Buffer size for reading data from client sockets.
+	MAX_BUFFER_SIZE = 4096
+	// SO_MAX_CONN is kernel parameter in Linux that determines the maximum number of connections that can be queued in the TCP/IP stack backlog per socket.
+	SO_MAX_CONN = 2048
+)

c243d57dfa338d5afd70fe519871fbfa317a31fd
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-06-29 13:52:51 +0530

    - Stop() to cancel all running go routines. - ListenAndServe() to create and listen to socket connections.

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,3 +26,38 @@ func New(port, maxListeners uint) *Server {
 		cancel:       cancel,
 	}
 }
+
+func (s *Server) ListenAndServe() error {
+	s.log.Printf("Server listening on port %d\n", s.port)
+
+	total := int(s.maxListeners)
+	s.wg.Add(total)
+
+	for i := range total {
+		go func() {
+			defer s.wg.Done()
+			s.startListening(i + 1)
+		}()
+	}
+
+	return nil
+}
+
+func (s *Server) Stop() error {
+	s.cancel()
+	s.wg.Wait()
+	return nil
+}
+
+func (s *Server) startListening(worker int) {
+	s.log.Printf("Starting worker with id %d\n", worker)
+
+	select {
+	case <-s.context.Done():
+		return
+	default:
+		{
+			// syscall.Socket(syscall.AF_INET)
+		}
+	}
+}
